f548aa55f60871819fd78256c15689eeecc1cbba
Elvis Acacio Barbosa <PUCMINAS\1050936@PPC01INFLL00126.pucminas.net>
2020-02-20 22:24:45 -0300

    #n: versao3.

--- main.rb
+++ main.rb
@@ -1,5 +1,39 @@
-puts "Entre com um palpite:"
-palpite = gets.chomp.to_i
-numero_secreto = 4
-puts "Voce acertou?"
-puts palpite == numero_secreto
+def inicio
+    puts "Bem-vindo ao jogo da adivinhacao"
+    end
+   
+    def nome
+    puts "Qual e o seu nome?"
+    nome = gets
+    puts "\n\n\n\n"
+    puts "Comecaremos o jogo para voce, " + nome
+    puts "Escolhendo um numero secreto entre 0 e 200..."
+    end
+    inicio
+    nome
+   
+    numero_secreto = 121
+    tent = 0
+    puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
+   
+    for i in 1..3
+       tent += 1
+    puts "\n\n\n"
+    puts "Tentativa " + tent.to_s
+    puts "Entre com o numero"
+    chute = gets
+    puts "Sera que acertou? Voce chutou " + chute
+    acertou = numero_secreto == chute.to_i
+        if acertou
+        puts "Acertou!"
+        else
+        maior = numero_secreto > chute.to_i
+        if maior
+        puts "O numero secreto e maior!"
+        else
+        puts "O numero secreto e menor!"
+        end
+        end
+       
+        
+   end
